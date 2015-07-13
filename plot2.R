cac053590ee1e235194337b65c934890d5ee9074
imshree <imshree@yahoo.com>
2015-07-13 14:22:11 -0700

    updated comment
    just to show update in recent time as I completed this last month but
    then since the timeline passed for submitting assignment dropped off
    from the course.

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-## getting full dataset
+## getting full dataset to process
 full_data <- read.csv("./household_power_consumption.txt", header=T, sep=";", 
                       na.strings="?", check.names=F, stringsAsFactors=F, 
                       comment.char="", quote='\"')
